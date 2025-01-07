735ebb93c216b86ad53c15b57204cc98e6bfec39
Crazzzy_lewis <liuyeweilewis@gmail.com>
2025-01-07 23:32:30 +0800

    basic framework

---
+++ smart_word_cloud/smart_word_cloud.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+# Author: Yewei Liu (Lewis) <liuyeweilewis@gmail.com> <2300012959@stu.pku.edu.cn>
+#
+# (c) 2025
+# License: MIT
+
+from __future__ import division
+
+class SmartWordCloud(object):
+    def __init__(self):
+        pass
+
+    def add_text(self, text, weight, focusing_words=None, focusing_weight=1, focusing_length=10):
+        pass
+
+    def add_stopword(self, word):
+        pass
+
+    def add_stopwords(self, words:list):
+        pass
+
+    def generate(self):
+        pass
